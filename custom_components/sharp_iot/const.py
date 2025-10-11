d0bf23f3f90a3399fa37533edfbdd56271440763
Jakub Fijałkowski <kuba@codinginfinity.me>
2025-10-11 21:44:25 +0200

    Automated HACS release build
    Generated from dev branch commit: 30b41b5

--- custom_components/sharp_iot/const.py
+++ custom_components/sharp_iot/const.py
@@ -7,6 +7,7 @@ CONF_TERMINAL_ID = "terminal_id"
 
 # Update intervals
 UPDATE_INTERVAL = 30  # seconds
+TERMINAL_REFRESH_INTERVAL = 86400  # seconds (24 hours)
 
 # Device info
 MANUFACTURER = "Sharp"
